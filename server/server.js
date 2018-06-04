1f3298f030aa4c7ef1b7137ead7dd1ee62385659
papergliff <papergliff@gmail.com>
2018-06-04 18:11:18 +0300

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -34,7 +34,6 @@ app.post('/todos', (req, res) => {
   })
 })
 
-// Get /todos/1234123
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id
 
@@ -89,6 +88,19 @@ app.patch('/todos/:id', (req, res) => {
 
     res.send({todo})
   }).catch(e => res.status(400).send())
+})
+
+app.post('/users', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password'])
+  const user = new User(body)
+
+  user.save().then(() => {
+    return user.generateAuthToken()
+  }).then(token => {
+    res.header('x-auth', token).send(user)
+  }).catch(e => {
+    res.status(400).send(e)
+  })
 })
 
 app.listen(port, () => {
