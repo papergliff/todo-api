8e0f02beb7e39e9401a5efd8818271865f378239
papergliff <papergliff@gmail.com>
2018-06-07 22:36:56 +0300

    Add tests for DELETE /users/me/token

--- server/server.js
+++ server/server.js
@@ -118,7 +118,7 @@ app.post('/users/login', (req, res) => {
   }).catch(e => res.status(400).send())
 })
 
-app.delete('/users/me/delete', authenticate, (req, res) => {
+app.delete('/users/me/token', authenticate, (req, res) => {
   req.user.removeToken(req.token).then(() => {
     res.status(200).send()
   }, () => {
